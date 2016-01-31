76b07087e423a8cbc0553d67910267d92a094786
Ankur Gupta <love2ankur+wyibe@gmail.com>
2016-01-31 14:40:48 +0530

    removing user not found bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 	def set_slug
 		o = [('a'..'z'), (1..9), ('A'..'Z')].map { |i| i.to_a }.flatten
 		string = (0...8).map { o[rand(o.length)] }.join
-		self.slug =  name.gsub(' ', '-') + '-' + user.id.to_s + string
+		self.slug =  name.gsub(' ', '-') + '-' + id.to_s + string
 		self.save
     end
 
